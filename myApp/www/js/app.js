7eae9c2ec9908d0a07821f4ad14a6096cc5199b9
Gurleen Phagura <gsp@Gurleens-Mac.local>
2016-10-17 13:08:14 -0700

    Small errors fixed in page9

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -32,23 +32,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.homepage', {
-      url: '/homepage',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/homepage.html'
-        }
-      }
-    })
   .state('app.browse', {
       url: '/browse',
       views: {
@@ -77,14 +60,126 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
+  .state('app.page1', {
+      url: '/page1',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page1.html'
+        }
+      }
+  })
+
+  .state('app.page2', {
+      url: '/page2',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page2.html'
+        }
+      }
+  }) 
+
+  .state('app.page3', {
+      url: '/page3',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page3.html'
+        }
+      }
+  }) 
+
+  .state('app.page4', {
+      url: '/page4',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page4.html'
+        }
+      }
+  }) 
+
+  .state('app.page5', {
+      url: '/page5',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page5.html'
+        }
+      }
+  }) 
+  .state('app.page6', {
+      url: '/page6',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page6.html'
+        }
+      }
+  }) 
+
+  .state('app.page7', {
+      url: '/page7',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page7.html'
+        }
+      }
+  }) 
+
+  .state('app.page8', {
+      url: '/page8',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page8.html'
+        }
+      }
+  }) 
+
   .state('app.page9', {
       url: '/page9',
       views: {
         'menuContent': {
-          templateUrl: 'templates/page9.html'
+          templateUrl: 'templates/page9.html',
+          controller: 'AssignmentsCtrl'
         }
       }
-    }) ;
+  }) 
+  .state('app.page10', {
+      url: '/page10',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page10.html'
+        }
+      }
+  }) 
+  .state('app.page11', {
+      url: '/page11',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page11.html'
+        }
+      }
+  }) 
+  .state('app.page12', {
+      url: '/page12',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page12.html'
+        }
+      }
+  }) 
+  .state('app.page13', {
+      url: '/page13',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page13.html'
+        }
+      }
+  }) 
+  .state('app.page14', {
+      url: '/page14',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/page14.html'
+        }
+      }
+  }) ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -53,4 +53,19 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('AssignmentsCtrl', function($scope) {
+  $scope.assignments = [
+    { title: 'Assignment1', id: 1 },
+    { title: 'Assignment2', id: 2 },
+    { title: 'Assignment3', id: 3 },
+    { title: 'Assignment4', id: 4 },
+    { title: 'Assignment5', id: 5 },
+        { title: 'Assignment2', id: 2 },
+    { title: 'Assignment3', id: 3 },
+    { title: 'Assignment4', id: 4 },
+    { title: 'Assignment5', id: 5 },
+    { title: 'Assignment6', id: 6 }
+  ];
 });
